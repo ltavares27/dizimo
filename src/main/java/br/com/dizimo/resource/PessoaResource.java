3707a69f1e08427d6aeadcb81190210d1d3b512a
ltavares27 <luispaulotav@gmail.com>
2019-01-23 14:32:35 -0200

    implementado os metodos endpoints do objeto Pessoa

--- src/main/java/br/com/dizimo/resource/PessoaResource.java
+++ src/main/java/br/com/dizimo/resource/PessoaResource.java
@@ -3,11 +3,12 @@ package br.com.dizimo.resource;
 import br.com.dizimo.model.Pessoa;
 import br.com.dizimo.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/pessoas")
 @RestController
@@ -16,7 +17,16 @@ public class PessoaResource  {
     @Autowired
     private PessoaService pessoaService;
 
-    @PostMapping("/save")
+    @GetMapping("/get/{idPessoa}") //READ
+    public ResponseEntity<?> getPessoa(@PathVariable("idPessoa") Integer id) {
+        if(id != null) {
+            Pessoa pessoa = pessoaService.findPessoaId(id);
+            return ResponseEntity.ok(pessoa);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/save") //CREATE
     public ResponseEntity<Pessoa> salvarPessao(@RequestBody Pessoa pessoa){
         Pessoa pessoaResponse = null;
         if(pessoa.getId() == null){
@@ -25,5 +35,32 @@ public class PessoaResource  {
             return ResponseEntity.ok(pessoaResponse);
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("/update/{idPessoa}") //UPDATE
+    public ResponseEntity<?> editarPessoas(@RequestBody Pessoa pessoaAtual, @PathVariable("idPessoa") Integer id) {
+       if(id != null){
+          Pessoa pessoaOld = pessoaService.findPessoaId(id);
+         if(pessoaOld != null) {
+            pessoaService.editarPessoa(pessoaOld, pessoaAtual);
+             return ResponseEntity.ok("Atualizado com Sucesso");
+         }
+       }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/delete/{idPessoa}") //DELETE
+    public ResponseEntity<?> deletarPessoa(@PathVariable("idPessoa") Integer id){
+       if(id != null) {
+          Pessoa pessoa = pessoaService.findPessoaId(id);
+          pessoaService.deletarPessoa(pessoa);
+           return ResponseEntity.ok("Deletado com Sucesso");
+       }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/List") //READ
+    public ResponseEntity<List<?>> getListAllDizimo(){
+        return new ResponseEntity<>(this.pessoaService.getAllPesssoas(), HttpStatus.OK);
     }
 }
--- src/main/java/br/com/dizimo/service/PessoaService.java
+++ src/main/java/br/com/dizimo/service/PessoaService.java
@@ -1,9 +1,12 @@
 package br.com.dizimo.service;
 
+import br.com.dizimo.model.Dizimo;
 import br.com.dizimo.model.Pessoa;
 import br.com.dizimo.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PessoaService {
@@ -20,5 +23,24 @@ public class PessoaService {
 
     public Pessoa findPessoaId(Integer id){
         return this.pessoaRepository.findPessoaById(id);
+    }
+
+    public List<Pessoa> getAllPesssoas() {
+        return this.pessoaRepository.findAll();
+    }
+
+    public void editarPessoa(Pessoa pessoaOld, Pessoa pessoaAtual) {
+        if(pessoaAtual != null && pessoaOld != null) {
+           pessoaOld.setNome(pessoaAtual.getNome());
+           pessoaOld.setCpf(pessoaAtual.getCpf());
+           pessoaOld.setDataNascimento(pessoaAtual.getDataNascimento());
+           pessoaOld.setTelefone(pessoaAtual.getTelefone());
+           pessoaOld.setEnderecos(pessoaAtual.getEnderecos());
+           pessoaRepository.save(pessoaOld);
+        }
+    }
+
+    public void deletarPessoa(Pessoa pessoa) {
+       this.pessoaRepository.delete(pessoa);
     }
 }
